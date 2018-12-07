3cef3420e81a773a454227a71e37e22e0809a7af
Judy Gonzalez <judy@thearrayschool.com>
2018-12-07 10:33:23 -0700

    If given an amount of cups, we can convert to gallons and round

--- answer.js
+++ answer.js
@@ -1,1 +1,12 @@
-// *** YOUR ANSWER BELOW ***
+const QUARTSTOGALLONS = 4
+const CUPSTOQUARTS = 4
+
+function convertingCupsToGallonsRounded(x){
+  let cup2Gallons = Math.round(x/16);
+  return cup2Gallons
+}
+
+
+
+
+convertingCupsToGallonsRounded(100);
